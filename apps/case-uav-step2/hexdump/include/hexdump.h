49a886de199805f2ef3313b03f3d1c3a732a636c
Karl Hoech <karl.hoech@collins.com>
2020-05-13 09:40:07 -0500

    Add ring hex dumper

--- apps/case-uav-step2/hexdump/include/hexdump.h
+++ apps/case-uav-step2/hexdump/include/hexdump.h
@@ -15,3 +15,11 @@ void fhexdump(FILE *stream, const char *prefix, size_t max_line_len, const uint8
 
 
 void hexdump(const char *prefix, size_t max_line_len, const uint8_t* data, size_t datalen);
+
+
+void fhexdump_ring(FILE *stream, const char *prefix, size_t max_line_len,
+		   const uint8_t* ring, size_t ring_size, size_t offset, size_t datalen);
+
+
+void hexdump_ring(const char *prefix, size_t max_line_len,
+		  const uint8_t* ring, size_t ring_size, size_t offset, size_t datalen);
