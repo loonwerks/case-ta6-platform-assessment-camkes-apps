e42a3e6724edfc3f49ee9f934b95cbb353c6f6be
Isaac Amundson <isaac.amundson@rockwellcollins.com>
2020-05-28 17:34:58 -0500

    uav step 5 added for insertion of attestation manager

---
+++ apps/case-uav-step5/components/WaypointManager/src/waypoint_manager.c
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2020, Collins Aerospace
+ */
+
+#include <camkes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <counter.h>
+#include <data.h>
+#include <queue.h>
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#include "hexdump.h"
+
+// Forward declarations
+/* int send_mission_command(void); */
+void mission_command_out_event_data_send(data_t *data);
+
+
+//------------------------------------------------------------------------------
+// User specified input data receive handler for AADL Input Event Data Port (in) named
+// "p1_in".
+void air_vehicle_state_in_event_data_receive(counter_t numDropped, data_t *data) {
+    printf("%s: received air vehicle state: numDropped: %" PRIcounter "\n", get_instance_name(), numDropped);
+    // hexdump("    ", 32, data->payload, sizeof(data->payload));
+}
+
+//------------------------------------------------------------------------------
+// Implementation of AADL Input Event Data Port (in) named "p1_in"
+// Three styles: poll, wait and callback.
+//
+// Callback would typically be avoid for safety critical systems. It is harder
+// to analyze since the callback handler is run on some arbitrary thread.
+//
+// NOTE: If we only need polling style receivers, we can get rid of SendEvent
+
+recv_queue_t airVehicleStateInRecvQueue;
+
+// Assumption: only one thread is calling this and/or reading p1_in_recv_counter.
+bool air_vehicle_state_in_event_data_poll(counter_t *numDropped, data_t *data) {
+    return queue_dequeue(&airVehicleStateInRecvQueue, numDropped, data);
+}
+
+// void air_vehicle_state_in_event_data_wait(counter_t *numDropped, data_t *data) {
+//     while (!air_vehicle_state_in_event_data_poll(numDropped, data)) {
+//         air_vehicle_state_in_SendEvent_wait();
+//     }
+// }
+
+// static void air_vehicle_state_in_handler(void *v) {
+//     counter_t numDropped;
+//     data_t data;
+// 
+//     // Handle ALL events that have been queued up
+//     while (air_vehicle_state_in_event_data_poll(&numDropped, &data)) {
+//         air_vehicle_state_in_event_data_receive(numDropped, &data);
+//     }
+//     while (! air_vehicle_state_in_SendEvent_reg_callback(&air_vehicle_state_in_handler, NULL));
+// }
+
+//---
+
+//------------------------------------------------------------------------------
+// User specified input data receive handler for AADL Input Event Data Port (in) named
+// "automation_response_in".
+void automation_response_in_event_data_receive(counter_t numDropped, data_t *data) {
+    printf("%s: received automation response: numDropped: %" PRIcounter "\n", get_instance_name(), numDropped);
+    // hexdump("    ", 32, data->payload, sizeof(data->payload));
+    // For testing, whenever we receive an automation response, send it out on the mission command out port
+    mission_command_out_event_data_send(data);
+}
+
+//------------------------------------------------------------------------------
+// Implementation of AADL Input Event Data Port (in) named "p1_in"
+// Three styles: poll, wait and callback.
+//
+// Callback would typically be avoid for safety critical systems. It is harder
+// to analyze since the callback handler is run on some arbitrary thread.
+//
+// NOTE: If we only need polling style receivers, we can get rid of SendEvent
+
+recv_queue_t automationResponseInRecvQueue;
+
+// Assumption: only one thread is calling this and/or reading p1_in_recv_counter.
+bool automation_response_in_event_data_poll(counter_t *numDropped, data_t *data) {
+    return queue_dequeue(&automationResponseInRecvQueue, numDropped, data);
+}
+
+// void automation_response_in_event_data_wait(counter_t *numDropped, data_t *data) {
+//     while (!automation_response_in_event_data_poll(numDropped, data)) {
+//         automation_response_in_SendEvent_wait();
+//     }
+// }
+
+// static void automation_response_in_handler(void *v) {
+//     counter_t numDropped;
+//     data_t data;
+// 
+//     // Handle ALL events that have been queued up
+//     while (automation_response_in_event_data_poll(&numDropped, &data)) {
+//         automation_response_in_event_data_receive(numDropped, &data);
+//     }
+//     while (! automation_response_in_SendEvent_reg_callback(&automation_response_in_handler, NULL));
+// }
+
+//--
+
+void done_emit_underlying(void) WEAK;
+static void done_emit(void) {
+  /* If the interface is not connected, the 'underlying' function will
+   * not exist.
+   */
+  if (done_emit_underlying) {
+    done_emit_underlying();
+  }
+}
+
+
+//------------------------------------------------------------------------------
+// Implementation of AADL Input Event Data Port (out) named "mission_command_out"
+//
+// NOTE: If we only need polling style receivers, we can get rid of the SendEvent
+
+// Assumption: only one thread is calling this and/or reading mission_command_out_recv_counter.
+void mission_command_out_event_data_send(data_t *data) {
+    queue_enqueue(mission_command_out_queue, data);
+    mission_command_out_SendEvent_emit();
+    done_emit();
+}
+
+
+static const char message[] = {
+// TODO: This message is BOGUS... Fix it.
+  0x61,0x66,0x72,0x6C,0x2E,0x63,0x6D,0x61,0x73,0x69,0x2E,0x4D,0x69,0x73,0x73,0x69,  /* afrl:cmasi:Missi */
+  0x6F,0x6E,0x43,0x6F,0x6D,0x6D,0x61,0x6E,0x6D,0x24,0x6C,0x6D,0x63,0x70,0x7C,0x61,  /* onCommand$lmcp|a */
+  0x66,0x72,0x6C,0x2E,0x63,0x6D,0x61,0x73,0x69,0x2E,0x4D,0x69,0x73,0x73,0x69,0x6F,  /* frl:cmasi:Missio */
+  0x6E,0x43,0x6F,0x6D,0x6D,0x61,0x6E,0x64,0x7C,0x54,0x63,0x70,0x42,0x72,0x69,0x64,  /* nCommand|6?p*rid */
+  0x67,0x65,0x7C,0x34,0x30,0x30,0x7C,0x36,0x38,0x24,0x4C,0x4D,0x43,0x50,0x00,0x00,  /* ge|400|68$LMCP.. */
+  0x00,0x2B,0x01,0x43,0x4D,0x41,0x53,0x49,0x00,0x00,0x00,0x00,0x00,0x00,0x27,0x00,  /* .+.CMASI......'. */
+  0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x50,0x00,0x01,0x00,0x00,0x00,0x00,0x00,  /* ........P....... */
+  0x00,0x01,0x4E,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x4F,0x00,0x00,0x03,  /* ..N.........O... */
+  0xE1                                                                              /* .                */
+};
+
+
+/*
+int send_mission_command(void) {
+    data_t data;
+
+    // Stage data
+    memcpy((void *) &data.payload[0], (const void *) &message[0], sizeof(message));
+    printf("%s: sending mission command: %d\n", get_instance_name(), sizeof(message));
+
+    // Send the data
+    mission_command_out_event_data_send(&data);          
+}
+*/
+
+//---
+
+//------------------------------------------------------------------------------
+// Testing - Three tests for the different styles: poll, wait and callback.
+//
+// NOTE: The constants in the tests were chosen to cause a variety of
+// situations at runtime including dropped packets and no data
+// available. These numbers may not cause the same variety of behaviour in
+// different test environments.
+
+void run_poll(void) {
+    counter_t numDropped;
+    data_t data;
+
+    while (true) {
+
+        bool dataReceived = air_vehicle_state_in_event_data_poll(&numDropped, &data);
+        if (dataReceived) {
+            air_vehicle_state_in_event_data_receive(numDropped, &data);
+        }
+
+        dataReceived = automation_response_in_event_data_poll(&numDropped, &data);
+        if (dataReceived) {
+            automation_response_in_event_data_receive(numDropped, &data);
+        }
+
+        seL4_Yield();
+    }
+
+}
+
+// void run_wait(void) {
+//     counter_t numDropped;
+//     data_t data;
+// 
+//     while (true) {
+//         // TODO: how to wait on either of two send events?
+//         air_vehicle_state_in_event_data_wait(&numDropped, &data);
+//         air_vehicle_state_in_event_data_receive(numDropped, &data);
+//         automation_response_in_event_data_wait(&numDropped, &data);
+//         automation_response_in_event_data_receive(numDropped, &data);
+//     }
+// }
+
+// int run_callback(void) {
+//      return p1_in_SendEvent_reg_callback(&p1_in_handler, NULL);
+// }
+
+void post_init(void) {
+    recv_queue_init(&airVehicleStateInRecvQueue, air_vehicle_state_in_queue);
+    recv_queue_init(&automationResponseInRecvQueue, automation_response_in_queue);
+    queue_init(mission_command_out_queue);
+}
+
+int run(void) {
+    // Pick one receive style to test.
+    run_poll();
+    //run_wait();
+    //return run_callback();
+}
+
---
+++ apps/case-uav-step5/vmUxAS/src/cross_vm_connections_vmUxAS.c
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2020, Collins Aerospace.
+ */
+
+#include <autoconf.h>
+#include <camkes.h>
+#include <vmlinux.h>
+#include <sel4vm/guest_vm.h>
+
+#include <sel4vmmplatsupport/drivers/cross_vm_connection.h>
+#include <sel4vmmplatsupport/drivers/pci_helper.h>
+#include <pci/helper.h>
+
+#ifdef CONFIG_PLAT_QEMU_ARM_VIRT
+#define CONNECTION_BASE_ADDRESS 0xDF000000
+#else
+#define CONNECTION_BASE_ADDRESS 0x3F000000
+#endif
+
+// The following device definitions correspond to the following camkes interfaces.
+//
+//    dataport queue_t operating_region_in_crossvm_dp;
+//    maybe consumes SendEvent operating_region_in_done;
+//
+//    dataport queue_t line_search_task_in_crossvm_dp;
+//    maybe consumes SendEvent line_search_task_in_done;
+//
+//    dataport queue_t automation_request_in_crossvm_dp;
+//    maybe consumes SendEvent automation_request_in_done;
+//
+//    dataport queue_t automation_response_out_crossvm_dp;
+//    emits SendEvent wpm_ready;
+//
+//    dataport queue_t crossvm_dp_apss;
+//    consumes SendEvent apss_done;
+//
+//    dataport queue_t uxas_log_out_crossvm_dp;
+//    emits SendEvent uxas_log_out_ready;
+
+
+#define NUM_CONNECTIONS 6
+static struct camkes_crossvm_connection connections[NUM_CONNECTIONS];
+
+// these are defined in the dataport's glue code
+extern dataport_caps_handle_t operating_region_in_crossvm_dp_handle;
+seL4_Word operating_region_in_done_notification_badge(void);
+
+extern dataport_caps_handle_t line_search_task_in_crossvm_dp_handle;
+seL4_Word line_search_task_in_done_notification_badge(void);
+
+extern dataport_caps_handle_t automation_request_in_crossvm_dp_handle;
+seL4_Word automation_request_in_done_notification_badge(void);
+
+extern dataport_caps_handle_t automation_response_out_crossvm_dp_handle;
+void automation_response_out_ready_emit_underlying(void); 
+
+extern dataport_caps_handle_t air_vehicle_state_in_crossvm_dp_handle;
+seL4_Word air_vehicle_state_in_done_notification_badge(void);
+
+extern dataport_caps_handle_t uxas_log_out_crossvm_dp_handle;
+void uxas_log_out_ready_emit_underlying(void); 
+
+
+static int consume_callback(vm_t *vm, void *cookie)
+{
+    consume_connection_event(vm, (seL4_Word) cookie, true);
+    return 0;
+}
+
+
+void init_cross_vm_connections(vm_t *vm, void *cookie)
+{
+    connections[0] = (struct camkes_crossvm_connection) {
+        .handle = &operating_region_in_crossvm_dp_handle,
+        .emit_fn = NULL,
+        .consume_badge = operating_region_in_done_notification_badge()
+    };
+
+    connections[1] = (struct camkes_crossvm_connection) {
+        .handle = &line_search_task_in_crossvm_dp_handle,
+        .emit_fn = NULL,
+        .consume_badge = line_search_task_in_done_notification_badge()
+    };
+
+    connections[2] = (struct camkes_crossvm_connection) {
+        .handle = &automation_request_in_crossvm_dp_handle,
+        .emit_fn = NULL,
+        .consume_badge = automation_request_in_done_notification_badge()
+    };
+
+    connections[3] = (struct camkes_crossvm_connection) {
+        .handle = &automation_response_out_crossvm_dp_handle,
+        .emit_fn = automation_response_out_ready_emit_underlying,
+        .consume_badge = -1
+    };
+
+    connections[4] = (struct camkes_crossvm_connection) {
+        .handle = &air_vehicle_state_in_crossvm_dp_handle,
+        .emit_fn = NULL,
+        .consume_badge = air_vehicle_state_in_done_notification_badge()
+    };
+
+    connections[5] = (struct camkes_crossvm_connection) {
+        .handle = &uxas_log_out_crossvm_dp_handle,
+        .emit_fn = uxas_log_out_ready_emit_underlying,
+        .consume_badge = -1
+    };
+
+    for (int i = 0; i < NUM_CONNECTIONS; i++) {
+        if (connections[i].consume_badge != -1) {
+            int err = register_async_event_handler(connections[i].consume_badge, consume_callback, (void *)connections[i].consume_badge);
+            ZF_LOGF_IF(err, "Failed to register_async_event_handler for init_cross_vm_connections.");
+
+        }   
+    }
+
+
+    cross_vm_connections_init(vm, CONNECTION_BASE_ADDRESS, connections, ARRAY_SIZE(connections));
+}
+
+DEFINE_MODULE(cross_vm_connections, NULL, init_cross_vm_connections)
