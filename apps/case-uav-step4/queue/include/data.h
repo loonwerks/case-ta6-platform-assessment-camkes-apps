54cbfd41534a68358f11d21f05e5f8cff218b582
Isaac Amundson <isaac.amundson@rockwellcollins.com>
2020-05-28 17:34:22 -0500

    uav step 4 added for insertion of high-assurance components

---
+++ apps/case-uav-step4/queue/include/data.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// The event data port type. Sender and receiver type must match. Many ports can use the same type.
+// The data representation is independent of the queue representation.
+//
+// NOTE: data_t must define a type for which assignment will copy all the
+// data. For example, pointers do not work.
+
+#include <stdint.h>
+#include <sys/types.h>
+
+#define DATA_T_MAX_PAYLOAD (8192 - sizeof(unsigned long long int))
+
+typedef struct data {
+  uint8_t payload[DATA_T_MAX_PAYLOAD];
+} data_t;
+
---
+++ apps/case-uav-step4/settings.cmake
@@ -0,0 +1,30 @@
+#
+# Copyright 2020, Collins Aerospace
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 3-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD3.txt" for details.
+#
+# Copied from an example by Data61
+
+# Add virtual PCI device to VMM for registering cross component connectors as
+# devices on the PCI bus.
+set(VmPCISupport ON CACHE BOOL "" FORCE)
+
+# Disable libusb from being compiled.
+set(LibUSB OFF CACHE BOOL "" FORCE)
+
+# Enables the option for the VM to open and load a seperate initrd file
+set(VmInitRdFile ON CACHE BOOL "" FORCE)
+
+# Enable virtio console vmm module
+set(VmVirtioConsole ON CACHE BOOL "" FORCE)
+
+# Make VTimers see absolute time rather than virtual time.
+set(KernelArmVtimerUpdateVOffset OFF CACHE BOOL "" FORCE)
+
+# Don't trap WFI or WFE instructions in a VM.
+set(KernelArmDisableWFIWFETraps ON CACHE BOOL "" FORCE)
+
+set(KernelNumDomains 3 CACHE STRING "" FORCE)
+set(KernelDomainSchedule "${CMAKE_CURRENT_LIST_DIR}/vm/domain_schedule.c" CACHE INTERNAL "")
