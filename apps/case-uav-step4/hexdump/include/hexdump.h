e893d9c81057545449b8445f146134ddc67f27f1
Isaac Amundson <isaac.amundson@rockwellcollins.com>
2020-05-28 17:59:49 -0500

    added raw output option for hex dump in step4

--- apps/case-uav-step4/hexdump/include/hexdump.h
+++ apps/case-uav-step4/hexdump/include/hexdump.h
@@ -16,6 +16,8 @@ void fhexdump(FILE *stream, const char *prefix, size_t max_line_len, const uint8
 
 void hexdump(const char *prefix, size_t max_line_len, const uint8_t* data, size_t datalen);
 
+void hexdump_raw(size_t max_line_len, const uint8_t* data, size_t datalen);
+
 
 void fhexdump_ring(FILE *stream, const char *prefix, size_t max_line_len,
 		   const uint8_t* ring, size_t ring_size, size_t offset, size_t datalen);
